6dde0ef7e9205f8267d38b1ee7281409caf3238e
민서 김 <dg4res@gmail.com>
2021-09-13 20:01:06 +0900

    1092 배 (GOLD V) 해결 완료

--- 1092 배/baekjoon_1092.cpp
+++ 1092 배/baekjoon_1092.cpp
@@ -11,52 +11,45 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	int N, M, high = 0;
-	vector<int> cMax;
+	int N;
 	cin >> N;
-	cMax.resize(N);
+	vector<int> crain;
 	for (int i = 0; i < N; i++) {
-		cin >> cMax[i];
-		if (high < cMax[i]) {
-			high = cMax[i];
-		}
+		int crain_Num;
+		cin >> crain_Num;
+		crain.push_back(crain_Num);
 	}
-	sort(cMax.begin(), cMax.end());
-	vector<int> cant;
-	cant.resize(N);
-	cant[N - 1] = 1;
-	for (int i = 0; i < cant.size() - 1; i++) {
-		cant[i] = cMax[i - 1] + 1;
-	}
+	int M;
 	cin >> M;
-	vector<int> kg;
-	kg.resize(M);
-	bool notPick = false;
+	vector<int> box;
 	for (int i = 0; i < M; i++) {
-		cin >> kg[i];
-		if (high < kg[i]) {
-			notPick = true;
-		}
+		int box_Num;
+		cin >> box_Num;
+		box.push_back(box_Num);
 	}
-	if (notPick == true) {
+	box.resize(M);
+	crain.resize(N);
+	sort(box.begin(), box.end(), compare);
+	sort(crain.begin(), crain.end(), compare);
+	if (crain[0] < box[0]) {
 		cout << "-1";
 		return 0;
 	}
-	sort(kg.begin(), kg.end(), compare);
-	int cnt = 0;
-	vector<int> x(0);
-	x.resize(N);
-	for (int i = 0; i < kg.size(); i++) {
-		if (kg[i] < cant[cnt]) {
-			cnt++;
-		}
-		x[cnt]++;
-	}
-	for (int i = 0; i < x.size(); i++) {
-		for (int j = i + 1; j < x.size(); j++) {
-			if (x[i] > x[j]) {
-				
+	int day = 0;
+	while (!box.empty()) {
+		day++;
+		for (int i = 0; i < crain.size(); i++) {
+			for (int j = 0; j < box.size(); j++) {
+				if (box[j] > crain[i]) {
+					continue;
+				}
+				else {
+					box.erase(box.begin() + j);
+					break;
+				}
 			}
 		}
 	}
+	cout << day;
+	return 0;
 }
